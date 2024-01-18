34f8265cecc26a69d409c6ac15f8590b89d007cd
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-18 20:58:36 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,69 +2,53 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <stack>
 #include <queue>
 #include <deque>
 #include <map>
+#include <list>
 #include <utility>
+#include <cmath>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
+typedef long long ll;
 using namespace std;
 
-// 1920
-
-int	N, M;
-vector<int>	A;
-vector<int>	C;
-
-void	bin_search(int low, int high, int target)	// using while loop
-{
-	while (low <= high)
-	{
-		int	mid = (low + high) / 2;
-		if (target == A[mid]) { cout << 1 << '\n'; return; }
-		if (target < A[mid]) { high = mid - 1; }
-		else if (target > A[mid]) { low = mid + 1; }
-	}
-	cout << 0 << '\n';
-}
-
-// void	bin_search_cur(int low, int high, int target)
-// {
-// 	if (low > high)	{ cout << 0 << '\n'; return; }
-// 	int	mid = (low + high) / 2;
-// 	if (A[mid] == target)	{ cout << 1 << '\n'; return; }
-// 	if (A[mid] > target)
-// 		return (bin_search_cur(low, mid - 1, target));
-// 	else
-// 		return (bin_search_cur(mid + 1, high, target));
-// }
+int N, M;
+vector<int> v;
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> N;
-	for (int i = 0; i < N; i++)
-	{
-		int	num;
-		cin >> num;
-		A.push_back(num);
-	}
-	
-	cin >> M;
-	for (int i = 0; i < M; i++)
-	{
-		int	num;
-		cin >> num;
-		C.push_back(num);
-	}
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	sort(A.begin(), A.end());
-	int	len = A.size();
-	for (int i = 0; i < M; i++)
-		bin_search(0, len - 1, C[i]);
-		// bin_search_cur(0, len - 1, C[i]);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int num; cin >> num;
+        v.push_back(num);
+    }
+    sort(v.begin(), v.end());
+    
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int target; cin >> target;
+        int st = 0;
+        int en = N - 1;
+        int c = 0;
+        while (st <= en)
+        {
+            int mid = (st + en) / 2;
+            if (target < v[mid]) {
+                en = mid - 1;
+            } else if (target > v[mid]) {
+                st = mid + 1;
+            } else {
+                c = 1;
+                break ;
+            }
+        }
+        cout << c << '\n';
+    }
 
-	return 0;
+    return 0;
 }
