7486ff4a8351330aee9a5ee0db1dfdeda8f569e5
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-25 17:54:42 +0900

    [Gold V] Title: 강의실 배정, Time: 60 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#include <cmath>
+#include <tuple>
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+typedef long long ll;
+using namespace std;
+
+int N;
+vector<pair<int, int>> vp;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+bool compare(pair<int,int> a, pair<int,int> b)
+{
+    return a.first < b.first;
+}
+
+int solve(int k)
+{
+    pq.push(vp[0].second);
+
+    for (int i = 1; i < vp.size(); i++) {
+        pq.push(vp[i].second);
+        if (pq.top() <= vp[i].first)
+            pq.pop();
+    }
+    return pq.size();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int x, y; cin >> x >> y;
+        vp.push_back({x, y});
+    }
+    sort(vp.begin(), vp.end(), compare);
+
+    cout << solve(N) << '\n';
+}
