a287392ded73a8f363c2db3d4787ef51467a86ca
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:15:45 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,29 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  x.begin(), x.end()
+using namespace std;
+
+void solve() {
+    int a, b; 
+    while(1) {
+        cin>>a>>b;
+        if (a==0)
+            break;
+        cout<<a+b<<'\n';
+    
+    }
+}
+int main()
+{   ios::sync_with_stdio(0);cin.tie(0);
+
+    // int tc; cin>>tc;
+    // while(tc--)
+        solve();
+
+    return 0;
+}
