355616f54fa522c16a87866098dd312cb0308aa2
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:15:15 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  x.begin(), x.end()
+using namespace std;
+
+int main()
+{   ios::sync_with_stdio(0);cin.tie(0);
+
+    int a, b; cin>>a>>b;
+    cout<<a+b<<'\n'<<a-b<<'\n'<<a*b<<'\n'<<a/b<<'\n'<<a%b<<'\n';
+
+    return 0;
+}
