056ecce55a4aa05e766901f804eb85c9f7f99815
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:17:22 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,24 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  x.begin(), x.end()
+using namespace std;
+
+void solve() {
+    char a; cin>>a;
+    cout<<(int) a;
+}
+
+int main()
+{   ios::sync_with_stdio(0);cin.tie(0);
+
+    // int tc; cin>>tc;
+    // while(tc--)
+        solve();
+
+    return 0;
+}
