0cdeaba11747990c7bc104de879195923dc035a5
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:12:38 +0900

    [Silver V] Title: Rectangle, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7113. Rectangle/Rectangle.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <list>
+#include <utility>
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+using namespace std;
+
+int sum;
+
+void    solve(int n, int m)
+{
+    if (n == 1 || m == 1) {
+        int mx = max(n, m);
+        sum += mx;
+        return ;
+    }
+
+    if (n > m)
+    {
+        n -= m;
+        sum++;
+        solve(n, m);
+    }
+    else if (m > n)
+    {
+        m -= n;
+        sum++;
+        solve(n, m);
+    }
+    else {
+        sum++;
+        return ;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N, M;
+    cin >> N >> M;
+
+    solve(N, M);
+    cout << sum;
+    return 0;
+}
