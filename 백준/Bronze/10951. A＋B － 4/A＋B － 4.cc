a23acc299dc5a35d6e6403d63b088901d83fb4ff
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:15:40 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,26 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  x.begin(), x.end()
+using namespace std;
+
+void solve() {
+    int a, b; 
+    while(cin>>a) {
+        cin>>b;
+        cout<<a+b<<'\n';
+    }
+}
+int main()
+{   ios::sync_with_stdio(0);cin.tie(0);
+
+    // int tc; cin>>tc;
+    // while(tc--)
+        solve();
+
+    return 0;
+}
