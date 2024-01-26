a46e2db6483ec53d6e7118d5469c307120953eec
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-26 22:16:13 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  x.begin(), x.end()
+using namespace std;
+
+int main()
+{   ios::sync_with_stdio(0);cin.tie(0);
+
+    double a, b;
+    cin>>a>>b;
+
+    cout<<fixed;
+    cout.precision(10);
+    cout<<a/b;
+
+    return 0;
+}
