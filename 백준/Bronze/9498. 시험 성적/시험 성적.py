8959ca2b811a0befbe003d5e7d0cbde1bd9e2151
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-27 00:13:44 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+a = input()
+
+if 90 <= int(a) <= 100:
+    print('A')
+elif 80 <= int(a) < 90:
+    print('B')
+elif 70 <= int(a) < 80:
+    print('C')
+elif 60 <= int(a) < 70:
+    print('D')
+else:
+    print('F') 
