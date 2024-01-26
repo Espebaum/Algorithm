fa92bd93c4f924611496a3db823efce2ef04cd9b
Espebaum/gyopark <104741009+Espebaum@users.noreply.github.com>
2024-01-26 22:54:58 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <deque>
+#define all(x)  (x).begin(), (x).end()
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);
+
+    int n;cin>>n;
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < i + 1; j++)
+            cout<<"*";
+        cout<<"\n";
+    }
+
+    return 0;
+}
