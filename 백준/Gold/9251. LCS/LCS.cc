9a53d9bd82dbbd2acdf9f849385fe7491ff15340
Espebaum/gyopark <vocops5000@gmail.com>
2024-02-27 10:54:11 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 6008 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,39 +1,28 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <list>
-#include <utility>
-#include <cmath>
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
-typedef long long ll;
+const int INF = 0x3f3f3f3f;
 using namespace std;
 
-string  a;
-string  b;
-int     dp[1010][1010];
+string s1;
+string s2;
+int dp[1010][1010];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    cin >> a;
-    cin >> b;
-
-    for (int i = 1; i <= a.length(); i++) {
-        for (int j = 1; j <= b.length(); j++) {
-            if (a[i - 1] != b[j - 1])
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    cin>>s1>>s2;
+    int len1 = s1.length(); // ACAYKP
+    int len2 = s2.length(); // CAPCAK
+    for (int i = 1; i <= len1; i++) {
+        for (int j = 1; j <= len2; j++) {
+            if (s1[i-1] == s2[j-1])
+                dp[i][j] = dp[i-1][j-1] + 1;
             else
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+                dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
         }
     }
-
-    cout << dp[a.length()][b.length()];
+    cout << dp[len1][len2];
     return 0;
 }
