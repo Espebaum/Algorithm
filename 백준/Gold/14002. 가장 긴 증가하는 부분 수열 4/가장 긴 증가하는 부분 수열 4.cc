a03b2504100236a92d0374af47217b6e2fceb636
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-21 21:58:22 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int N;
 int arr[1010];
-int dp[1010][1010];
+int dp[1010];
 
 int main()
 {
@@ -17,35 +17,34 @@ int main()
     for (int i = 1; i <= N; i++)
         cin >> arr[i];
 
-    dp[1][0] = 1;
-    dp[1][1] = arr[1];
+    dp[1] = 1;
 
     for (int i = 2; i <= N; i++) 
     {
         int mx = -1;
         for (int j = 1; j < i; j++)
             if (arr[j] < arr[i])
-                mx = max(mx, dp[j][0]);
+                mx = max(mx, dp[j]);
         if (mx != -1)
-            dp[i][0] = mx + 1;
+            dp[i] = mx + 1;
         else
-            dp[i][0] = 1;
+            dp[i] = 1;
     }
 
     int val = -1;
     for (int i = 1; i <= N; i++)
-        val = max(val, dp[i][0]);
+        val = max(val, dp[i]);
 
     cout << val << '\n';
     vector<int>ans;
     for (int i = N; i >= 1; i--) {
-        if (dp[i][0] == val) 
+        if (dp[i] == val) 
         {
             if (ans.empty()) 
                 ans.push_back(arr[i]);
             else if (!ans.empty()) 
             {
-                if (dp[i][0] > ans[ans.size() - 1])
+                if (dp[i] > ans[ans.size() - 1])
                     continue ;
                 else
                     ans.push_back(arr[i]);
