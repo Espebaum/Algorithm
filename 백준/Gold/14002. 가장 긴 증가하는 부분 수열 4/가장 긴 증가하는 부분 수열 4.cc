9dd7198c273046613f6a67abe5f5610abe5dce01
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-21 21:56:15 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 6008 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+const int INF = 0x3f3f3f3f;
+using namespace std;
+
+int N;
+int arr[1010];
+int dp[1010][1010];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        cin >> arr[i];
+
+
+    dp[1][0] = 1; // mx length
+    dp[1][1] = arr[1]; // mx value
+
+    for (int i = 2; i <= N; i++) 
+    {
+        int mx = -1;
+        for (int j = 1; j < i; j++) {
+            if (arr[j] < arr[i])
+                mx = max(mx, dp[j][0]);
+        }
+        if (mx != -1) {
+            dp[i][0] = mx + 1;
+        }
+        else {
+            dp[i][0] = 1;
+        }
+    }
+
+    int val = -1;
+
+    for (int i = 1; i <= N; i++) {
+        val = max(val, dp[i][0]);
+    }
+
+    cout << val << '\n';
+    vector<int>ans;
+    for (int i = N; i >= 1; i--) {
+        if (dp[i][0] == val) 
+        {
+            if (ans.empty()) 
+            {
+                ans.push_back(arr[i]);
+            }
+            else if (!ans.empty()) 
+            {
+                if (dp[i][0] > ans[ans.size() - 1])
+                    continue ;
+                else
+                    ans.push_back(arr[i]);
+            }
+            val -= 1;
+        }
+    }
+
+    for (int i = ans.size() - 1; i >= 0; i--) {
+        cout << ans[i] << ' ';
+    }   cout << '\n';
+
+    return 0;
+}
