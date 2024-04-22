e762660e8d0f7d8dad03af5b9191d3950fb8ce88
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-22 16:44:30 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,38 +1,33 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <queue>
-#include <deque>
-#include <map>
-#include <utility>
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
+const int INF = 0x3f3f3f3f;
 using namespace std;
 
-//2579
-
-int stair[303];
+int N;
+int arr[303];
 int dp[303];
-int n;
-
-//0
-//10 20
-//
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    cin>>n;
-    stair[0] = 0;
-    for (int i = 0; i < n; i++)
-        cin>>stair[i];
-    dp[0] = stair[0];
-    dp[1] = stair[0] + stair[1];
-    dp[2] = max(stair[0] + stair[2], stair[1] + stair[2]);
-    for (int i = 3; i < n; i++)
-        dp[i] = max(dp[i - 3] + stair[i - 1] + stair[i], dp[i - 2] + stair[i]);
-    cout<<dp[n - 1];
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+        cin >> arr[i];
+
+    if (N == 1) {
+        cout << arr[1];
+        return 0;
+    }
+
+    dp[1] = arr[1];
+
+    for (int i = 2; i <= N; i++) {
+        dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
+    }
+
+    cout << dp[N];
+
     return 0;
 }
