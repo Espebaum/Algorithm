2c91cc6c7575f96daeeda03993541744c49a07ff
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-22 16:44:59 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -16,13 +16,7 @@ int main()
     for (int i = 1; i <= N; i++)
         cin >> arr[i];
 
-    if (N == 1) {
-        cout << arr[1];
-        return 0;
-    }
-
     dp[1] = arr[1];
-
     for (int i = 2; i <= N; i++) {
         dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]);
     }
