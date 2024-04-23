adb3f2421cbcf55920ccd513f9c6c55c8deb17f1
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-23 16:32:18 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -16,23 +16,15 @@ int main()
 
     for (int i = 1; i <= N; i++)
         cin >> arr[i];
-
-    dp[1] = arr[1];
         
+    dp[1] = arr[1];
     int mx = -1;
     for (int i = 2; i <= N; i++) {
         for (int j = 1; j < i; j++) {
-            // cout << dp[i - j] + arr[j] << '\n';
-            // cout << arr[i] << '\n';
             mx = max({dp[i - j] + arr[j], arr[i], mx});
         }
         dp[i] = mx;
     }
-
-    // for (int i = 1; i <= N; i++)
-    //     cout << dp[i] << ' ';
-    // cout << '\n';
-
     cout << dp[N];
     return 0;
 }
