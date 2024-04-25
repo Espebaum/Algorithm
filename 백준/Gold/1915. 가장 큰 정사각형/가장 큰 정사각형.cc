8d652da028c810c0bbad5f7a2fc1b481bbc4a36a
Espebaum/gyopark <vocops5000@gmail.com>
2024-04-25 15:12:50 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 472 ms, Memory: 7000 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -10,13 +10,9 @@ int dp[1010][1010];
 int mx;
 int nr, nc, mn;
 
-void    check(int x, int y) // 3 4
+void    check(int x, int y)
 {
-    // cout << "x: " << x << " y: " << y << '\n';
-    int cnt = dp[x - 1][y - 1]; // 2
-    // cout << "cnt: " << cnt << '\n';
-
-
+    int cnt = dp[x - 1][y - 1];
     nr = 0; nc = 0; mn = 0;
     for (int i = 0; i <= cnt; i++) {
         if (board[x - i][y] != '0')
@@ -24,19 +20,14 @@ void    check(int x, int y) // 3 4
         else
             break ;
     }
-
     for (int i = 0; i <= cnt; i++) {
         if (board[x][y - i] != '0')
             nc++;
         else
             break ;
     }
-
-    // cout << "nr: " << nr << " nc: " << nc << '\n';
-
-    if (nr == nc && nr == cnt + 1) {
+    if (nr == nc && nr == cnt + 1)
         dp[x][y] = dp[x - 1][y - 1] + 1;
-    }
     else
         dp[x][y] = min(nr, nc);
 }
@@ -47,35 +38,17 @@ int main()
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
             cin >> board[i][j];
-    }
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            if (board[i][j] == '0')
-                dp[i][j] = 0;
-            else
-                check(i, j);
+            if (board[i][j] == '0') dp[i][j] = 0;
+            else check(i, j);
             mx = max(mx, dp[i][j]);
         }
     }
-
-    // cout << "BOARD\n";
-    // for (int i = 1; i <= n; i++) {
-    //     for (int j = 1; j <= m; j++)
-    //         cout << board[i][j] << ' ';
-    //     cout << '\n';
-    // }
-    // cout << "\nDP\n";
-    // for (int i = 1; i <= n; i++) {
-    //     for (int j = 1; j <= m; j++)
-    //         cout << dp[i][j] << ' ';
-    //     cout << '\n';
-    // }
-    
     cout << mx * mx << '\n';
-
     return 0;
 }
