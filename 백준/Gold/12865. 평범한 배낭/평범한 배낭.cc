1a731b89b6388cc2333881a97d228a74afa3c49a
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-10 15:31:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 36 ms, Memory: 44996 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,51 +1,32 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <list>
-#include <utility>
-#include <cmath>
-#include <tuple>
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
-typedef long long ll;
+const int INF = 0x3f3f3f3f;
 using namespace std;
 
 int N, K;
-int dp[102][100001];
-int w[102];
-int v[102];
-
-void    solve()
-{
-    for (int lim = 1; lim <= K; lim++) {
-        for (int row = 1; row <= N; row++) {
-            if (w[row] > lim)
-                dp[row][lim] = dp[row - 1][lim];
-            else
-                dp[row][lim] = max(dp[row - 1][lim - w[row]] + v[row], dp[row - 1][lim]);
-        }
-    }
-}
+int w[110];
+int v[110];
+int dp[110][100010];
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     cin >> N >> K;
-
     for (int i = 1; i <= N; i++) {
         cin >> w[i] >> v[i];
     }
 
-    solve();
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= K; j++) {
+            if (w[i] <= j)
+                dp[i][j] = max(dp[i - 1][j - w[i]] + v[i], dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
 
     cout << dp[N][K];
-
     return 0;
 }
