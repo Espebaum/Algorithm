20592885e4e42e33456651400322c1ab845ec7e7
Espebaum/gyopark <vocops5000@gmail.com>
2024-05-10 18:24:13 +0900

    [Platinum IV] Title: 평범한 배낭 2, Time: 56 ms, Memory: 400832 KB -BaekjoonHub

---
+++ 백준/Platinum/12920. 평범한 배낭 2/평범한 배낭 2.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+const int INF = 0x3f3f3f3f;
+using namespace std;
+
+int N, M; // 물건갯수, 최대수용무게
+int v[10101]; // 무게
+int c[10101]; // 만족도
+int k[10101]; // 개수
+vector<pair<int,int>>vp;
+int dp[10101][10101];
+vector<int>W;
+vector<int>V;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+        cin >> v[i] >> c[i] >> k[i];
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = k[i]; j >= 1; j >>= 1) {
+            int num = j - (j >> 1);
+            vp.push_back({v[i]*num, c[i]*num});
+        }
+    }
+
+    int nn = vp.size();
+    W.resize(nn+1);
+    V.resize(nn+1);
+    for (int i = 1; i <= nn; i++) {
+        W[i] = vp[i-1].first;
+        V[i] = vp[i-1].second;
+    }
+
+    for (int i = 1; i <= nn; i++) {
+        for (int j = 0; j <= M; j++) {
+            if (W[i] <= j)
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-W[i]] + V[i]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+    cout << dp[nn][M];    
+
+    return 0;
+}
