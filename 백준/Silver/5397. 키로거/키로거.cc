f3fdb67de5d9b74d9f4332f1a3d1c3decdde8155
Espebaum/gyopark <vocops5000@gmail.com>
2024-10-07 20:22:25 +0900

    [Silver II] Title: 키로거, Time: 236 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.cc
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -1,51 +1,52 @@
-// #include <bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <list>
-// #include <queue>
-// #include <deque>
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
-#define sz(x) (int)(x).size()
+#define int long long
+const int INF = 0x3f3f3f3f;
 using namespace std;
-using vi = vector<int>;
 
-void solve() {
-    string raw;
-    cin>>raw;
-    
-    list<char> a;
-    auto cur = a.end();
-    for(auto c:raw){
-        if(c == '<'){
-            if(cur != a.begin()) cur--;
+int tc;
+
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> tc;
+
+    while (tc--)
+    {
+        list<char> d;
+        string s; cin >> s;
+        auto cursor = d.begin();
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '<') {
+                if (cursor != d.begin())
+                    cursor--;
+            }
+            else if (s[i] == '>') {
+                if (cursor != d.end())
+                    cursor++;
+            }
+            else if (s[i] == '-') {
+                if (cursor != d.begin()) {
+                    cursor = d.erase(prev(cursor));
+                }
+            }
+            else {
+                d.insert(cursor, s[i]);
+            }
+            // cout << "word: " << s[i] << '\n';
+            // cout << "cursor: " << cursor << '\n';
+            // for (int i = 0; i < s.length(); i++)
+            //     cout << (char) d[i];
+            // cout <<'\n';          
         }
-        else if(c == '>'){
-            if(cur != a.end()) cur++;
+        for (list<char>::iterator it = d.begin(); it != d.end(); ++it)
+        {
+            cout << *it;
         }
-        else if(c == '-'){
-            if(cur != a.begin())
-                cur = a.erase(--cur);
-        }
-        else{
-            a.insert(cur, c);
-        }
+        cout << '\n';
     }
-    for (auto c:a){
-        cout<<c;
-    }
-    cout<<'\n';
-}
-
-
-int main()
-{   
-    ios::sync_with_stdio(0);cin.tie(0);
-
-    int tc; cin>>tc;
-    while(tc--)
-        solve();
     return 0;
 }
