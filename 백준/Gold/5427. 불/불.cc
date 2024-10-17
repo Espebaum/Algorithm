a8db686bd2b5b1dbaf82f282ea62efc96a7bb544
Espebaum/gyopark <vocops5000@gmail.com>
2024-10-18 00:08:12 +0900

    [Gold IV] Title: 불, Time: 60 ms, Memory: 13128 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.cc
+++ 백준/Gold/5427. 불/불.cc
@@ -1,95 +1,90 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <list>
-#include <utility>
+#include <bits/stdc++.h>
 #define all(x) (x).begin(), (x).end()
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
+// #define int long long
+const int INF = 0x3ff3f3f;
 using namespace std;
 
 int tc;
-int n, m;
+int w, h;
+char board[1010][1010];
+int fire[1010][1010];
+int dog[1010][1010];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
-string board[1010];
-int dist1[1010][1010]; // fire
-int dist2[1010][1010]; // sanggeun
-
-void    solve()
-{
-    cin>> m >> n;
-
-    queue< pair<int, int> > q1; // fire
-    queue< pair<int, int> > q2; // sanggeun
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> board[i];
-        for(int j = 0; j < m; j++)
-        {
-            dist1[i][j] = -1; dist2[i][j] = -1;
-            if (board[i][j] == '*') {
-                q1.push(make_pair(i, j));
-                dist1[i][j] = 0;
-            }
-            if (board[i][j] == '@') {
-                q2.push(make_pair(i, j));
-                dist2[i][j] = 0;
-            }
-        }
-    }
-
-    while (!q1.empty())
-    {
-        pair<int, int> cur = q1.front(); q1.pop();
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-                continue;
-            if (board[nx][ny] == '#' || dist1[nx][ny] >= 0)
-                continue;
-            dist1[nx][ny] = dist1[cur.first][cur.second] + 1;
-            q1.push(make_pair(nx, ny));
-        }
-    }
-
-    while (!q2.empty())
-    {
-        pair<int, int> cur = q2.front(); q2.pop();
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                cout << dist2[cur.first][cur.second] + 1 << '\n';
-                return ;
-            }
-            if (board[nx][ny] == '#' || dist2[nx][ny] >= 0)
-                continue;
-            if (dist1[nx][ny] != -1 && (dist1[nx][ny] <= dist2[cur.first][cur.second] + 1))
-                continue;
-            dist2[nx][ny] = dist2[cur.first][cur.second] + 1;
-            q2.push(make_pair(nx, ny));   
-        }
-    }
-    cout << "IMPOSSIBLE" << '\n';
-}
+int flag;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-    cin>>tc;
+    cin >> tc;
 
     while (tc--)
-        solve();
+    {
+        flag = 0;
+        queue<pair<int,int>>f;
+        queue<pair<int,int>>s;
+        cin >> w >> h;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                fire[i][j] = -1;
+                dog[i][j] = -1;
+            }
+        }
+        int sx, sy;
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                cin >> board[i][j];
+                if (board[i][j] == '@') {
+                    s.push({i, j});
+                    dog[i][j] = 0;
+                } else if (board[i][j] == '*') {
+                    f.push({i, j});
+                    fire[i][j] = 0;
+                }
+            }
+        }
+        while (!f.empty())
+        {
+            pair<int,int>cur = f.front(); f.pop();
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int nx = cur.first + dx[dir];
+                int ny = cur.second + dy[dir];
+                if (nx < 0 || nx >= h || ny < 0 || ny >= w)
+                    continue ;
+                if (board[nx][ny] == '#' || fire[nx][ny] >= 0)
+                    continue;
+                fire[nx][ny] = fire[cur.first][cur.second] + 1;
+                f.push({nx, ny});
+            }
+        }
+        while (!s.empty())
+        {
+            pair<int,int>cur = s.front(); s.pop();
+            if (cur.first == h - 1 || cur.second == w - 1 || cur.first == 0 || cur.second == 0) {
+                flag = 1;
+                cout << dog[cur.first][cur.second] + 1 << '\n';
+                break ;
+            }
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int nx = cur.first + dx[dir];
+                int ny = cur.second + dy[dir];
+                if (nx < 0 || nx >= h || ny < 0 || ny >= w)
+                    continue ;
+                if (board[nx][ny] == '#' 
+                    || (fire[nx][ny] != -1 && fire[nx][ny] <= dog[cur.first][cur.second] + 1) 
+                        || dog[nx][ny] != -1)
+                    continue ;
+                dog[nx][ny] = dog[cur.first][cur.second] + 1;
+                s.push({nx, ny});
+            }
+        }
+        if (!flag)
+            cout << "IMPOSSIBLE\n";
+    }
 
     return 0;
 }
